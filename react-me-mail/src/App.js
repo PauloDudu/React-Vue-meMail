1e52198515bdeea6a3ba3f0c42dc3847c763235a
PauloDudu <dudu.magnificat@gmail.com>
2020-11-28 17:08:28 -0300

    Creating react app

---
+++ react-me-mail/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Koe tia Claudia! acabei de mudar pra vc ver hehehe
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
