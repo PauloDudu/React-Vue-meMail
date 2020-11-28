07c2264f5315bb76463503c213fbf4b1d9e4adc1
PauloDudu <dudu.magnificat@gmail.com>
2020-11-28 17:39:19 -0300

    Initial commit react me-MAil

--- react-me-mail/src/App.js
+++ react-me-mail/src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './Routes';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Koe tia Claudia! acabei de mudar pra vc ver hehehe
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
+    </>
+
   );
 }
 
